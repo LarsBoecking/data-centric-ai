0a789a7e7d1551a8d55fc7a9a511d59d7cc7fd2f
LarsBoecking <boecking.lars@googlemail.com>
2025-07-23 10:13:14 +0200

    Add configuration files for experiments and paths; refactor main script to utilize ConfigHandler

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from src.utils import CONFIG_PATH, DATASETS_PATH, ensure_datasets_exist, load_and_expand_yaml, logger, RESULTS_DIR
-from src.experiment import Experiment
+from src.utils.configHandler import ConfigHandler
+from src.experiments.experiment import Experiment
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
 
 if __name__ == "__main__":
-    ensure_datasets_exist()
-    configs = load_and_expand_yaml(CONFIG_PATH)
+
+    _config_handler = ConfigHandler()
+
+    _config_handler.ensure_datasets_exist()
+    configs = _config_handler.load_and_expand_yaml(_config_handler.CONFIG_PATH)
 
     with logging_redirect_tqdm():
         for config in tqdm(configs, desc="Experiments", unit="config"):
-            experiment = Experiment(config, base_path=DATASETS_PATH, results_root=RESULTS_DIR)
+            experiment = Experiment(
+                config,
+                base_path=_config_handler.DATASETS_PATH,
+                results_root=_config_handler.RESULTS_DIR,
+                summary_file=_config_handler.SUMMARY_FILE,
+            )
             experiment.run()
-        logger.info("All experiments completed")
+        _config_handler.logger.info("All experiments completed")

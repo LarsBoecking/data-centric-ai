5c085c923746e1d42a023afeeba21329be34234c
LarsBoecking <boecking.lars@googlemail.com>
2026-02-24 08:57:45 +0100

    Enhance dataset management: add dataset download and extraction; refactor paths in main execution flow

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-import os 
-from src.utils import load_and_expand_yaml, logger, RESULTS_DIR
+from src.utils import CONFIG_PATH, DATASETS_PATH, ensure_datasets_exist, load_and_expand_yaml, logger, RESULTS_DIR
 from src.experiment import Experiment
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
-if __name__ == "__main__":
-    config_path = "experiment.yaml"
-    base_path = "112UCRFolds"
 
-    os.makedirs(RESULTS_DIR, exist_ok=True)
-    configs = load_and_expand_yaml(config_path)
+if __name__ == "__main__":
+    ensure_datasets_exist()
+    configs = load_and_expand_yaml(CONFIG_PATH)
 
     with logging_redirect_tqdm():
         for config in tqdm(configs, desc="Experiments", unit="config"):
-            experiment = Experiment(config, base_path=base_path, results_root=RESULTS_DIR)
+            experiment = Experiment(config, base_path=DATASETS_PATH, results_root=RESULTS_DIR)
             experiment.run()
-            logger.info("")
         logger.info("All experiments completed")
